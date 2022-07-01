95d0499d9ab1ae6345f28a3a5d58b29fffca9853
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-01 20:58:10 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,48 @@
+# for j in range (5):
+#     print(j)   # the output may come in the vertical 
+
+# p=range(8)
+# for j in p:
+#     print(j,end=' ')  # the output may come in the horizontal     #  0 1 2 3 4 5 6 7
+
+# for r in range(1,5):
+#     print(r)
+
+# for p in range(4,5):
+#     print(p)   #    4
+
+# for q in range(2,2):
+#     print(q)      # we don't get anything in the output
+
+# for g in range(0,10,1):
+#     print(g)
+
+# for h in range(0,10,2):
+#     print(h)    # the output may get in even numbers and in vertical
+
+# for s in range(1,10,2):
+#     print(s)    #  the output may get in odd numbers and in vertical
+
+# for f in range(2,10,2):
+#     print(f)    # even numbers 
+
+# for k in range(10,0,-1):
+#     print(k)  # the output will come 10 to 1 
+
+# for d in range(10,1,-1):
+#     print(d)  # the output will come 10 to 2
+
+# for w in range(10,0,-2):
+#     print(w)   # the output will come from 10 to 2 and it is even order
+
+# for odd in range(1,30,2):
+#     print(odd,end=' ')   #  1 3 7 9 11 13 15 17 19 21 23 25 27 29
+
+
+# z=[1,2,3,4]   it is  a list type
+# # print(z)  # [1, 2, 3, 4]
+# print(*z)   # 1 2 3 4    when we give a (*) .then, it don't contain any bracket and any comma
+
+# y=1,6,9,4   # it is a int type
+# print(y)   #  (1, 6, 9, 4)
+# print(*y)   #  1 6 9 4   when we give a (*) .then, it don't contain any bracket and any comma
